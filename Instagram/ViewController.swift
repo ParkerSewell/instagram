e77ca5142feeb7446c8348e905176e1b66e51005
JMeyer333 <joanna.meyer333@utexas.edu>
2016-01-28 17:50:32 -0600

    instagram
    my first instagram

--- Instagram/ViewController.swift
+++ Instagram/ViewController.swift
@@ -13,7 +13,41 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        
+        let apiKey = "e05c462ebd86446ea48a5af73769b602"
+        let url = NSURL(string:"https://api.instagram.com/v1/media/popular?client_id=e05c462ebd86446ea48a5af73769b602api_key=\(apiKey)")
+        
+        let request = NSURLRequest(URL: url!)
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate:nil,
+            delegateQueue:NSOperationQueue.mainQueue()
+        )
+        
+        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
+            completionHandler: { (dataOrNil, response, error) in
+                if let data = dataOrNil {
+                    if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                        data, options:[]) as? NSDictionary {
+                            
+                            print("response: \(responseDictionary)")
+                            //hide HUD
+                       
+                            //MBProgressHUD.hideHUDForView(self.view, animated: true)
+                            
+                           // self.movies = responseDictionary["results"] as? [NSDictionary]
+                            //self.tableView.reloadData()
+                      
+                            
+                            
+                    }
+                }
+        });
+        task.resume()
+        
+
+                    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
